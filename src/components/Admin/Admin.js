1600cb617ec281c2560faf8c2fd4bb4100f57893
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-08-27 17:04:36 +0600

    added isAdminLoggedIn feature

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,27 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, Spinner } from "react-bootstrap";
+import { UserContext } from "../../App";
 import AddProduct from "../AddProduct/AddProduct";
 import Header from "../Header/Header";
 import ManageProduct from "../ManageProduct/ManageProduct";
 import "./Admin.css";
 
 const Admin = () => {
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   const [adminStatus, setAdminStatus] = useState({
     manageProduct: false,
     addProduct: false,
     editProduct: false,
   });
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [spinner, setSpinner] = useState(true);
 
   const [products, setProducts] = useState([]);
   const [productsLength, setProductsLength] = useState(products?.length);
-  
-    useEffect(() => {
-        fetch('https://nameless-spire-32810.herokuapp.com/products')
-        .then(res => res.json())
-        .then(data => {
-          setProducts(data);
-        })
-    }, [productsLength])
+
+  useEffect(() => {
+    fetch("https://nameless-spire-32810.herokuapp.com/isAdmin?email=" + loggedInUser.email)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("admin good?", data);
+        setIsAdmin(data);
+        setSpinner(false);
+      });
+  }, [loggedInUser.email]);
+
+  useEffect(() => {
+    fetch("https://nameless-spire-32810.herokuapp.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, [productsLength]);
 
   const handleProduct = (status) => {
     const newAdminStatus = { ...adminStatus };
@@ -46,40 +60,63 @@ const Admin = () => {
     <>
       <Header></Header>
       <section className="App mt-5">
-        <div className="container admin">
-          <Button
-            onClick={() => handleProduct(1)}
-            className="ml-4 mr-4 admin-btn"
-          >
-            Manage Product
-          </Button>
-          <Button
-            onClick={() => handleProduct(2)}
-            className="ml-4 mr-4 admin-btn"
-          >
-            Add Product
-          </Button>
-          <Button
-            onClick={() => handleProduct(3)}
-            className="ml-4 mr-4 admin-btn"
-          >
-            Edit Product
-          </Button>
-        </div>
-        <div className='mt-5'>
-          {adminStatus.manageProduct && 
-          <div className='container'>
-            <p className='manage-product-title'> 
-              <span style={{display: 'inline-block', width: '40%'}}>Product Name</span>
-              <span style={{display: 'inline-block', width: '20%'}}>ID</span>
-              <span style={{display: 'inline-block', width: '20%'}}>Price</span>
-              <span style={{display: 'inline-block', width: '20%'}}>Action</span>
-            </p>
-            {
-              products?.map((product => <ManageProduct product={product} productsLength={productsLength} setProductsLength={setProductsLength}></ManageProduct>))
-            }
+        { spinner &&
+          <Spinner className="mt-5" animation="border" role="status">
+          <span className="visually-hidden"></span>
+        </Spinner>
+        }
+        {!spinner && isAdmin.length ? (
+          <div className="container admin">
+            <Button
+              onClick={() => handleProduct(1)}
+              className="ml-4 mr-4 admin-btn"
+            >
+              Manage Product
+            </Button>
+            <Button
+              onClick={() => handleProduct(2)}
+              className="ml-4 mr-4 admin-btn"
+            >
+              Add Product
+            </Button>
+            <Button
+              onClick={() => handleProduct(3)}
+              className="ml-4 mr-4 admin-btn"
+            >
+              Edit Product
+            </Button>
           </div>
-          }
+        ) : !spinner && (
+          <div className="mt-5">
+            <h1>Only admin can access this section!</h1>
+          </div>
+        )}
+        <div className="mt-5">
+          {adminStatus.manageProduct && (
+            <div className="container">
+              <p className="manage-product-title">
+                <span style={{ display: "inline-block", width: "40%" }}>
+                  Product Name
+                </span>
+                <span style={{ display: "inline-block", width: "20%" }}>
+                  ID
+                </span>
+                <span style={{ display: "inline-block", width: "20%" }}>
+                  Price
+                </span>
+                <span style={{ display: "inline-block", width: "20%" }}>
+                  Action
+                </span>
+              </p>
+              {products?.map((product) => (
+                <ManageProduct
+                  product={product}
+                  productsLength={productsLength}
+                  setProductsLength={setProductsLength}
+                ></ManageProduct>
+              ))}
+            </div>
+          )}
           {adminStatus.addProduct && <AddProduct></AddProduct>}
           {adminStatus.editProduct && <h1>Edit Product</h1>}
         </div>
